f61e818ce0cfd0ee5cbe5a98c97fd8f02eff28c1
Pascal Berski <github@blackflagtv.de>
2024-05-30 14:56:06 +0200

    chore: Add health bar slider and update enemy health in FightingHandler.cs

--- Assets/Scripts/FightingHandler.cs
+++ Assets/Scripts/FightingHandler.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FightingHandler : MonoBehaviour
 {
     public static double enemyHealth = 100;
     public GameObject enemy;
+    public static double maxHealth = 100;
+    public static Slider healthSlider;
 
     // Start is called before the first frame update
     void Start()
@@ -13,6 +16,14 @@ public class FightingHandler : MonoBehaviour
         if (enemy == null)
         {
             enemy = GameObject.FindWithTag("enemy");
+        }
+
+        GameObject healthSliderObject = GameObject.FindWithTag("health_bar");
+        if (healthSliderObject != null)
+        {
+            healthSlider = healthSliderObject.GetComponent<Slider>();
+            healthSlider.maxValue = (float)maxHealth;
+            healthSlider.value = (float)enemyHealth;
         }
     }
 
@@ -24,12 +35,24 @@ public class FightingHandler : MonoBehaviour
             Destroy(enemy); // Entfernt den Gegner aus der Szene
             Debug.Log("Gegner besiegt!");
         }
+
+        if (healthSlider != null)
+        {
+            healthSlider.value = (float)enemyHealth;
+        }
     }
 
     public static void hit_with_card(CardType card) {
         double damage = CalculateDamage(card);
         enemyHealth -= damage;
+        if (enemyHealth > maxHealth) enemyHealth = maxHealth;
         Debug.Log("Gegner getroffen! Verbleibende Gesundheit: " + enemyHealth);
+
+        if (healthSlider != null)
+        {   
+            Debug.Log("Healthslider: " + healthSlider);
+            healthSlider.value = (float)enemyHealth;
+        }
     }
 
     private static double CalculateDamage(CardType card)
